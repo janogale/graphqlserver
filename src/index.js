500724454b4c7d34d4778cebcf7245fe0e9eb7a4
janagale <mukhtar.mahomed@gmail.com>
2022-01-25 12:24:54 +0300

    Update port

--- src/index.js
+++ src/index.js
@@ -14,10 +14,10 @@ const server = new ApolloServer({
   },
 });
 
-server.listen().then(() => {
+server.listen({port: process.env.PORT || 4000}).then(() => {
   console.log(`
     🚀  Server is running!
-    🔉  Listening on port 4000
+    🔉  Listening on port ${process.env.PORT || 4000}
     📭  Query at https://studio.apollographql.com/dev
   `);
 });
